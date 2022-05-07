9d2aab5ca9b858446304e49d1456626b88a75a05
thunder410426 <1164410836@qq.com>
2022-05-08 01:30:23 +0800

    项目创建

---
+++ src/main/java/com/example/masksale/MaskSaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.masksale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaskSaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/masksale/MaskSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masksale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaskSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
